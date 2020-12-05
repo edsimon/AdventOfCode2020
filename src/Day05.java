66835cadfd6e23a1fa6da44b1eef43d420770c95
edsimon <simon.edvards@gmail.com>
2020-12-05 09:14:31 +0100

    Solution for day 5, cool recursion this day :)

--- src/Day05.java
+++ src/Day05.java
@@ -1,8 +1,6 @@
 import Helpers.Reader;
-
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
